c2550914b0855bdef4b354b8cd79d56f0e07f7e0
Juan Marcos Gutierrez Ramos <juangura@gmail.com>
2022-01-21 21:49:03 -0400

    DEV: Create pages according to sitemap

---
+++ src/pages/first-time-buyers.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function Firsttimebuyers() {
+  return (
+    <Layout>
+      <div>
+        <section>
+          <div>
+            <h1>First Time Buyers</h1>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
+}
---
+++ src/pages/services/buyers.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Buyers() {
+  return <div>Buyers page</div>;
+}
