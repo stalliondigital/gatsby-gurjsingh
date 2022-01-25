484fa8dd3ca4c61d26e7e5aea8cfc7e32f5c334a
Juan Marcos Gutierrez Ramos <juangura@gmail.com>
2022-01-25 11:11:20 -0400

    DEV: Create Metadata, add plugin to optimize files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+
+  siteMetadata: {
+    title: "Gurjivan Singh",
+    description: "Professional Realtor &copy;",
+  },
 }
