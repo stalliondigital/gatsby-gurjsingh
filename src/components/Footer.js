56b258686bb78799ec407b6f32d621f44fbfd9fa
Juan Marcos Gutierrez Ramos <juangura@gmail.com>
2022-01-27 20:29:08 -0400

    DEV: Add footer basic content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,20 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 import React from "react"
 import logo from "./../images/gurjsingh-logo.png"
 import mls_logo from "./../images/mls.png"
+import footerImg from "./../images/footer_image.png"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons"
+import {
+  faPhoneAlt,
+  faEnvelope,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons"
 
 export default function Footer() {
   const data = useStaticQuery(graphql`
@@ -22,23 +36,52 @@ export default function Footer() {
       <div className="container">
         <div className="row py-5">
           <div className="col-6 col-lg-3">
-            <Link className="" to="/">
-              <img src={logo} className="h-10" alt="" />
-              <span className="navbar-text">
-                {title}
-                {description}
-              </span>
+            <Link className="text-reset" to="/">
+              <div className="row row-cols-auto">
+                <div className="col-3 pe-0 ">
+                  <img src={logo} className="h-10" alt="" />
+                </div>
+                <div className="col ps-1">
+                  <span className="navbar-text">
+                    {title}
+                    <br />
+                    {description}
+                  </span>
+                </div>
+              </div>
             </Link>
             <p>
               Whether it being finding a new home or selling your existing home,
               I’m here to help you every step of the way.
             </p>
+            <div className="row">
+              <div className="col-8 d-flex justify-content-around">
+                <FontAwesomeIcon icon={faPhoneAlt} />
+                <FontAwesomeIcon icon={faEnvelope} />
+                <FontAwesomeIcon icon={faFacebook} />
+                <FontAwesomeIcon icon={faInstagram} />
+                <FontAwesomeIcon icon={faTwitter} />
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </div>
+            </div>
           </div>
           <div className="col-6 col-lg-3">
-            <p>RE/MAX</p>
+            <img src={footerImg} className="h-4" alt="" />
             <p>RE/MAX 2000 REALTY</p>
-            <p>#103-15127 100th Avenue, Surrey, BC V3R 0N9 </p>
-            <p>778-951-8275</p>
+            <div className="row">
+              <div className="col-1">
+                <FontAwesomeIcon icon={faMapMarkerAlt} />
+              </div>
+              <div className="col-11">
+                #103-15127 100th Avenue,
+                <br />
+                Surrey, BC V3R 0N9
+              </div>
+              <div className="col-1">
+                <FontAwesomeIcon icon={faPhoneAlt} />
+              </div>
+              <div className="col-11">778-951-8275</div>
+            </div>
           </div>
           <div className="col-12 col-lg-6">
             <div className="row">
@@ -87,22 +130,46 @@ export default function Footer() {
                   </li>
                 </ul>
               </div>
-              <div className="col-4"></div>
+              <div className="col-4">
+                <p>Know Gurj</p>
+                <ul className="list-unstyled">
+                  <li>
+                    <Link className="text-reset" to="#">
+                      Our Process
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="text-reset" to="/about">
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="text-reset" to="#">
+                      Community
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="text-reset" to="#">
+                      Testimonials
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-        <div className="row py-5">
-          <div className="col-12 col-lg-6">
+        <div className="row py-5 align-items-center">
+          <div className="col-12 col-lg-6 order-2 order-lg-1 text-center pt-6">
             <p>© 2022 Gurjivan Singh. All Rights Reserved</p>
             <p>Made by Stallion Digital</p>
           </div>
-          <div className="col-12 col-lg-6">
+          <div className="col-12 col-lg-6 order-1 order-lg-2">
             <div className="container justify-content-center align-items-center">
               <div className="row justify-content-center align-items-center">
                 <div className="col-2">
-                  <img src={mls_logo} className="h-10" alt="" />
+                  <img src={mls_logo} className="img-fluid w-auto" alt="" />
                 </div>
-                <div className="col-10 small text-muted">
+                <div className="col-10 small text-muted text-center">
                   The data relating to real estate on this web site comes in
                   part from the MLS® Reciprocity program of the Real Estate
                   Board of Greater Vancouver or the Fraser Valley Real Estate
