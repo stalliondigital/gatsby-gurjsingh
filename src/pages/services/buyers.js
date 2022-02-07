04cfdab0eedef88f47225f70ddfdaef465967b0b
Juan Marcos Gutierrez Ramos <juangura@gmail.com>
2022-02-07 10:41:12 -0400

    DEV: Add content and style for buyers page

--- src/pages/services/buyers.js
+++ src/pages/services/buyers.js
@@ -5,6 +5,12 @@ import backgroundimage from "./../../images/bs-background.png"
 import sol_1 from "./../../images/sol-1.png"
 import sol_2 from "./../../images/sol-2.png"
 import sol_3 from "./../../images/sol-3.png"
+import prep_1 from "./../../images/prep-1.png"
+import prep_2 from "./../../images/prep-2.png"
+import prep_3 from "./../../images/prep-3.png"
+import prep_4 from "./../../images/prep-4.png"
+import prep_5 from "./../../images/prep-5.png"
+import prep_6 from "./../../images/prep-6.png"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons"
@@ -33,7 +39,7 @@ export default function Home() {
                   <div className="col-6 mt-10 d-grid pe-0">
                     <Link
                       to="/first-time-buyers"
-                      className="btn btn-block btn-primary bg-yellow-500 border-none shadow-sm mx-2"
+                      className="primary btn btn-block border-none shadow-sm mx-2"
                     >
                       First Time Buyers
                     </Link>
@@ -41,7 +47,7 @@ export default function Home() {
                   <div className="col-6 mt-10 d-grid ps-0">
                     <Link
                       to="/services/sellers"
-                      className="btn btn-block btn-neutral border-none mx-2"
+                      className="secondary btn btn-block border-none mx-2"
                     >
                       I am a seller
                     </Link>
@@ -51,10 +57,10 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="solution container-fluid text-lg-start pb-5 pt-lg-6 align-items-center pt-10">
+        <section className="solution container-fluid text-lg-start align-items-center pt-10 pb-15">
           <div className=" container-lg max-w-screen-xl   mb-5 ">
             <div className="row justify-content-center">
-              <div className="col-5">
+              <div className="col-8 col-lg-5">
                 <h2 className="ls-tight font-bolder display-5 text-white text-center mb-2 ">
                   Our Three Phase <br></br>Solution to Success
                 </h2>
@@ -74,7 +80,7 @@ export default function Home() {
                     <img
                       src={sol_1}
                       className="card-img-top"
-                      alt="Card two people discussing"
+                      alt="two people discussing"
                     />
                   </div>
                   <div className="card-body row align-items-center ">
@@ -93,7 +99,7 @@ export default function Home() {
                     <img
                       src={sol_2}
                       className="card-img-top"
-                      alt="Card a person talking on the phone"
+                      alt="a person talking on the phone"
                     />
                   </div>
                   <div className="card-body d-flex flex-row align-items-center">
@@ -112,7 +118,7 @@ export default function Home() {
                     <img
                       src={sol_3}
                       className="card-img-top"
-                      alt="Card a person next to a for sale sign"
+                      alt="a person next to a for sale sign"
                     />
                   </div>
                   <div className="card-body d-flex flex-row align-items-center">
@@ -138,7 +144,7 @@ export default function Home() {
             </span>
           </h2>
         </div>
-        <section className="phases container-fluid text-lg-start pb-5 pt-lg-6 align-items-center pt-10">
+        <section className="phases container-fluid text-lg-start align-items-center pt-20">
           <div className=" container-lg max-w-screen-xl   mb-5 ">
             <div className="container">
               <h2 className="phase text-center fs-5 fw-light">PHASE ONE</h2>
@@ -146,7 +152,7 @@ export default function Home() {
                 Preparing You to Buy
               </h2>
               <div className="row justify-content-center">
-                <div className="col-6">
+                <div className="col-8 col-lg-6">
                   <p className="description text-white fw-light text-center lh-sm fs-5 ">
                     Our first step toward success will be to discuss & plan
                     exactly what you need from your home to best position you
@@ -155,6 +161,288 @@ export default function Home() {
                 </div>
               </div>
             </div>
+          </div>
+          <div className="container container-fluid ">
+            {/* PART: We will discover  */}
+            <div className="row d-flex align-items-center justify-content-center ">
+              <div className="col-9 col-lg-5 pb-7 pb-lg-2">
+                <img
+                  src={prep_1}
+                  className="img-fluid pe-lg-7"
+                  alt="three people discussing"
+                />
+              </div>
+              <div className="col-12 col-lg-7">
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  We will discover exactly what you are looking for, go through,
+                  plan and discuss expecting closing costs when buying a home,
+                  ensure you are taken care of with a preapproval and set up
+                  everything we need for success when we are ready to enter the
+                  market.
+                </p>
+              </div>
+            </div>
+            {/* PART: Our Onboarding Consultation */}
+            <div className="py-20 row row align-items-center justify-content-center">
+              <div className="col-12 col-lg-9">
+                <h3 className="text-white fs-4 pb-3 ">
+                  Our Onboarding Consultation
+                </h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  Our initial 1-hour meeting will be used to unearth everything
+                  you need and want from your home. We will further discuss
+                  initial information regarding current market conditions, share
+                  the benefits and obligations when working with a professional
+                  REALTOR© and set all expectations needed to best position you
+                  for success!
+                </p>
+              </div>
+            </div>
+
+            {/* PART: Estimation of Expenses & Closing Costs */}
+            <div className="row align-items-center justify-content-center ">
+              <div className="d-flex align-items-center justify-content-center col-9 col-lg-4 pb-7 pb-lg-2">
+                <img
+                  src={prep_2}
+                  className="img-fluid pe-lg-6 "
+                  alt="money and gold coins"
+                />
+              </div>
+              <div className="col-12 col-lg-8">
+                <h3 className="text-white fs-4 pb-3 ">
+                  Estimation of Expenses & Closing Costs
+                </h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  A massive part my clients success is ensuring they have all
+                  the necessary information ahead of time so there are no mix
+                  ups or surprises when moving forward with buying your dream
+                  home. Apart of this is transparently sharing the associated
+                  costs involved with the purchase of your home and proactively
+                  preparing so we can make the entire process be as smooth as
+                  possible.
+                </p>
+              </div>
+            </div>
+
+            {/* PART: Getting Preapproved */}
+            <div className="pt-20 row align-items-start justify-content-start ">
+              <div className="col-12 col-lg-9">
+                <h3 className="text-white fs-4 pb-3 ">Getting Preapproved</h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  Having a preapproval done before we hit the market will allow
+                  us to strike immediately on opportunities when they arise as
+                  well as will strengthen any of our offers we put forward. Gurj
+                  can assist in arranging an appointment or meeting with a
+                  mortgage or finance professional who will can help you
+                  financially plan for the long term as well as pre-qualify you
+                  for your property.
+                </p>
+              </div>
+            </div>
+
+            {/* end phases */}
+          </div>
+        </section>
+        {/* break line */}
+        <div className="break container-fluid">
+          <h2></h2>
+        </div>
+
+        {/* PHASE TWO */}
+        <section className="phases container-fluid text-lg-start align-items-center pt-20">
+          <div className=" container-lg max-w-screen-xl  mb-5 ">
+            <div className="container">
+              <h2 className="phase text-center fs-5 fw-light">PHASE TWO</h2>
+              <h2 className="ls-tight font-bolder display-5 text-white text-center mb-2 ">
+                Preparing You to Buy
+              </h2>
+              <div className="row justify-content-center">
+                <div className="col-8 col-lg-6">
+                  <p className="description text-white fw-light text-center lh-sm fs-5 ">
+                    The Difference lays within our process of serving you.
+                    Gurjivan takes pride in his ability to adapt his services
+                    and share information in ways that's most convenient and
+                    hassle free for his clients.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container container-fluid pt-20">
+            {/* PART: We will discover  */}
+            <div className="row align-items-center justify-content-center ">
+              <div className="d-flex align-items-center justify-content-center col-9 col-lg-5 pb-7 pb-lg-2">
+                <img
+                  src={prep_3}
+                  className="img-fluid pe-lg-5"
+                  alt="a person writing on a board"
+                />
+              </div>
+              <div className="col-12 col-lg-7">
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  Everything within the scope of your home search including home
+                  tours, in-depth market analysis, explaining contracts,
+                  negotiations and the entire home transaction process will have
+                  Gurj there to explain and walk through the entire process to
+                  ensure you, the client always understand what is going on
+                  while instilling confidence throughout.
+                </p>
+              </div>
+            </div>
+            {/* PART: Our Onboarding Consultation */}
+            <div className="py-20 row align-items-start justify-content-start ">
+              <div className="col-12 col-lg-9">
+                <h3 className="text-white fs-4 pb-3 ">Proactive Monitoring</h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  After our initial sit down, when you’re ready to enter the
+                  market, we will take all the information initially discussed
+                  and put it to work. Gurj will set up a system for you that
+                  will make life simple and easy by having a search engine
+                  proactively pull listings and homes for you to review. With
+                  your feedback of what you like or did not like, the engine
+                  will narrow it’s search until we are consistently landing on
+                  homes that are serious contenders for us to move forward on.
+                  Using information as a tool will allow us to find and close on
+                  your dream home a quick as possible.
+                </p>
+              </div>
+            </div>
+
+            {/* PART: Estimation of Expenses & Closing Costs */}
+            <div className="row align-items-center justify-content-center ">
+              <div className="d-flex align-items-center justify-content-center col-9 col-lg-4 pb-7 pb-lg-2">
+                <img
+                  src={prep_4}
+                  className="img-fluid pe-lg-7"
+                  alt="a hand holding a flashlight"
+                />
+              </div>
+              <div className="col-12 col-lg-8">
+                <h3 className="text-white fs-4 pb-3 ">
+                  Home Tours & Explorations
+                </h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  Find a home that meets your use case and passes the eye test
+                  is only part one of the equation. The next step will always be
+                  to get in the home and objectively look at and evaluate the
+                  property in person. Gurj will be there to show you what to
+                  look for in the home, what small details we should keep an eye
+                  out for, and what clues we can pick up on that will give us a
+                  deeper insight of the property.
+                </p>
+              </div>
+            </div>
+
+            {/* PART: Getting Preapproved */}
+            <div className="py-20 row align-items-start justify-content-start ">
+              <div className="col-12 col-lg-9">
+                <h3 className="text-white fs-4 pb-3 ">
+                  Market Analysis & Due Diligence
+                </h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  We always do our homework so we can always make the smart
+                  decision. Regardless of your budget or personal situation, for
+                  any homes you seriously consider, we will prepare a full
+                  evaluation of the property, look at the long & short term
+                  plans of the associated community & then plan and discuss
+                  pricing strategy to close in on your dream home.
+                </p>
+              </div>
+            </div>
+
+            {/* PART: Estimation of Expenses & Closing Costs */}
+            <div className="row align-items-center justify-content-center ">
+              <div className="col-9 col-lg-4 pb-7 pb-lg-2 d-flex align-items-center justify-content-center ">
+                <img
+                  src={prep_5}
+                  className="img-fluid pe-lg-7"
+                  alt="a hand checking a list"
+                />
+              </div>
+              <div className="col-12 col-lg-8">
+                <h3 className="text-white fs-4 pb-3 ">The Paperwork</h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  After strategizing and planning the home we want to close in
+                  on, Gurj will carefully review and present our offer while
+                  having an open channel of communication available to further
+                  negotiate the terms of the contract to best position your
+                  interest as a buyer while ensuring we don’t run into any
+                  untimely surprises.
+                </p>
+              </div>
+            </div>
+
+            {/* PART: Getting Preapproved */}
+            <div className="pt-20 row align-items-start justify-content-start ">
+              <div className="col-12 col-lg-9">
+                <h3 className="text-white fs-4 pb-3 ">
+                  Get the Champagne Ready
+                </h3>
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  After our offer comes back to us accepted, Gurj will handle
+                  the rest of the organization that needs to be done for us to
+                  hand you over the keys. From time of acceptance to possession,
+                  a few tasks will need to be completed between the brokerage
+                  and related parties that Gurj will stay on top of and provide
+                  you daily updates on. For you, all that needs to be done is to
+                  relax and unwind; the tough part is over.
+                </p>
+              </div>
+            </div>
+
+            {/* end phases */}
+          </div>
+        </section>
+
+        {/* break line */}
+        <div className="break container-fluid">
+          <h2></h2>
+        </div>
+
+        {/* PHASE THREE */}
+        <section className="phases container-fluid text-lg-start pb-5 pt-20 align-items-center ">
+          <div className=" container-lg max-w-screen-xl   mb-5 ">
+            <div className="container">
+              <h2 className="phase text-center fs-5 fw-light">PHASE THREE</h2>
+              <h2 className="ls-tight font-bolder display-5 text-white text-center mb-2 ">
+                Welcoming You to Your New Home
+              </h2>
+              <div className="row justify-content-center">
+                <div className="col-8 col-lg-6">
+                  <p className="description text-white fw-light text-center lh-sm fs-5 ">
+                    The most difficult and stressful part of the process is
+                    over. Now we can start shift our energy from finding you the
+                    house you love, to making your new property your perfect
+                    home.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container container-fluid py-20">
+            {/* PART: Estimation of Expenses & Closing Costs */}
+            <div className="row align-items-center justify-content-center ">
+              <div className="col-9 col-lg-4 pb-7 pb-lg-2 d-flex align-items-center justify-content-center ">
+                <img
+                  src={prep_6}
+                  className="img-fluid pe-lg-7"
+                  alt="a smiley face"
+                />
+              </div>
+              <div className="col-12 col-lg-8">
+                <p className="description text-white fw-light lh-sm fs-5 ">
+                  When we get confirmation that your deal goes through and your
+                  new home is officially yours, Gurj will assist with additional
+                  tools and benefits to make post-possession as seamless as
+                  possible. Some benefits include but are not limited to, a full
+                  move-in checklist, contacts for furnishing & electronics
+                  outlets at discounted prices, moving service referrals,
+                  contacts for priority service installations and more.
+                </p>
+              </div>
+            </div>
+
+            {/* end phases */}
           </div>
         </section>
       </div>
