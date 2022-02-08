ece7d5729117767887cf2b23a9207e8c00d8cd45
Juan Marcos Gutierrez Ramos <juangura@gmail.com>
2022-02-08 15:07:58 -0400

    DEV: Create contact content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,8 @@ import Layout from "../components/Layout"
 import sol_1 from "./../images/contact-1.png"
 import sol_2 from "./../images/contact-2.png"
 import sol_3 from "./../images/contact-3.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowAltCircleRight as farArrow } from "@fortawesome/free-regular-svg-icons"
 export default function Contact() {
   return (
     <Layout>
@@ -23,7 +25,7 @@ export default function Contact() {
           </div>
           <div className="card-container container align-items-center ">
             <div className="row justify-content-center row-cols-1 row-cols-lg-3">
-              <div className="col-7 col-lg-4 mb-3 justify-content-center ">
+              <div className="contact-card col-7 col-lg-4 mb-3 justify-content-center ">
                 <a className="text-reset" href="tel:778-951-8275">
                   <div className="card p-4 h-100 ">
                     <div className="card-img p-4">
@@ -44,7 +46,7 @@ export default function Contact() {
                   </div>
                 </a>
               </div>
-              <div className="col-7 col-lg-4 mb-3 justify-content-center">
+              <div className="contact-card col-7 col-lg-4 mb-3 justify-content-center">
                 <a className="text-reset" href="mailto:gurj@gurjsingh.ca">
                   <div className="card p-4 h-100">
                     <div className="card-img p-4">
@@ -65,7 +67,7 @@ export default function Contact() {
                   </div>
                 </a>
               </div>
-              <div className="col-7 col-lg-4 mb-3 justify-content-center">
+              <div className="contact-card col-7 col-lg-4 mb-3 justify-content-center">
                 <a
                   className="text-reset"
                   href="https://twitter.com/getgurj"
@@ -91,6 +93,59 @@ export default function Contact() {
                 </a>
               </div>
             </div>
+          </div>
+        </section>
+        <section className="contact-request container-fluid text-lg-start pb-10 align-items-center ">
+
+          <div className="container-request container container-lg max-w-screen-lg mb-5 rounded-3 py-5 pb-10 ">
+            <form
+              method="post"
+              netlify-honeypot="bot-field"
+              data-netlify="true"
+              name="meeting"
+              className="g-3 text-center justify-content-center"
+            >
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="row justify-content-center pb-5">
+                <div className="col col-lg-7">
+                  <h2 className="ls-tight font-bolder display-5 text-white ">
+                    Book your first meeting with Gurjivan
+                  </h2>
+                </div>
+              </div>
+              <div className="form-data row justify-content-center  d-flex align-items-center  g-3 px-10">
+                <div className="col-9 col-lg-3">
+                  <input
+                    type="text"
+                    className="form-control text-warning bg-white bg-opacity-50 border-0"
+                    placeholder="Name"
+                    aria-label="Name"
+                  />
+                </div>
+                <div className="col-9 col-lg-3">
+                  <input
+                    type="number"
+                    className="form-control text-warning bg-white bg-opacity-50 border-0"
+                    placeholder="Phone Number"
+                    aria-label="Phone Number"
+                  />
+                </div>
+                <div className="col-9 col-lg-3">
+                  <input
+                    type="email"
+                    className="form-control text-warning bg-white bg-opacity-50 border-0"
+                    placeholder="Email Address"
+                    aria-label="Email Address"
+                  />
+                </div>
+                <div className="arrow-icon col-9 col-lg-1 ">
+                  <button className="py-0" type="submit">
+                    <FontAwesomeIcon icon={farArrow} className="fa-icon" />
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </section>
       </div>
