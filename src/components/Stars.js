9d685359f535ffc880176aa1b5d2e6aaa947397d
Juan Marcos Gutierrez Ramos <juangura@gmail.com>
2022-04-04 23:13:56 -0400

    DEV: Modify stars fontawesome component

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,13 +2,16 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
 export default function Stars() {
+  const stars = [1, 2, 3, 4, 5]
   return (
-    <span className="text-end pe-3 fs-6">
-      <FontAwesomeIcon className="font-awesome" icon={faStar}></FontAwesomeIcon>{" "}
-      <FontAwesomeIcon className="font-awesome" icon={faStar}></FontAwesomeIcon>{" "}
-      <FontAwesomeIcon className="font-awesome" icon={faStar}></FontAwesomeIcon>{" "}
-      <FontAwesomeIcon className="font-awesome" icon={faStar}></FontAwesomeIcon>{" "}
-      <FontAwesomeIcon className="font-awesome" icon={faStar}></FontAwesomeIcon>
+    <span>
+      {stars.map(star => (
+        <FontAwesomeIcon
+          className="font-awesome experience-star"
+          transform="shrink-4"
+          icon={faStar}
+        ></FontAwesomeIcon>
+      ))}
     </span>
   )
 }
