75289e60c822128307b3d2c59e9e5e57e9ee38a0
jumagura <juangura@gmail.com>
2022-04-30 10:35:32 -0400

    DEV: Add hidden field to form to specify client request

--- src/pages/about.js
+++ src/pages/about.js
@@ -318,6 +318,7 @@ export default function About() {
             >
               <input type="hidden" name="bot-field" onChange={handleChange} />
               <input type="hidden" name="form-name" value="buyerkit" />
+              <input type="hidden" name="type" value="Book a meeting" />
               <div className="row py-5">
                 <div className="col d-flex flex-column flex-lg-row row">
                   <div className="col col-lg-7 px-10 justify-content-center d-flex flex-column align-items-center align-items-lg-start">
